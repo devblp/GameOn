e53cd35249ff00b3d373d643a458fc351c95c8f6
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-06-21 06:57:16 -0700

    create nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from "./Pages/Home";
+import SideNavbar from "./Components/SideNavbar"
 
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SideNavbar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </>
-  )
+  );
 }
-
-export default App
